7d1616f619011a3c12290c20b9b26bef19d9ed3f
Benjamin Cardenas <benjaminc5751@gmail.com>
2022-07-26 18:55:48 -0500

    Second Commit
    Added Calculator functionality

---
+++ public/js/calculator.js
@@ -0,0 +1,30 @@
+function buttonNum(num) {
+    document.getElementById("result").value += num
+}
+
+// Clears the results
+function clearResult() {
+    document.getElementById("result").value =""
+}
+
+function calculateResult() {
+    let num = document.getElementById("result").value 
+    let result = eval(num)
+
+    document.getElementById("result").value = result
+}
+
+function plusMinus() {
+    let num = document.getElementById("result").value 
+    let result = eval(num * -1)
+
+    document.getElementById("result").value = result
+}
+
+function percentage() {
+    let num = document.getElementById("result").value 
+    let result = eval(num / 100)
+
+    document.getElementById("result").value = result
+}
+
